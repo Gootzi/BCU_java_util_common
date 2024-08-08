c8fe4f51fe08b7e6462672dff8e6e1d5c2843481
iRed <foodrickme@gmail.com>
2024-08-07 18:37:42 -0700

    adjust price & cooldown to colo limits

--- battle/ELineUp.java
+++ battle/ELineUp.java
@@ -3,6 +3,7 @@ package common.battle;
 import common.CommonStatic;
 import common.util.BattleObj;
 import common.util.stage.Limit;
+import common.util.unit.Form;
 
 public class ELineUp extends BattleObj {
 
@@ -15,16 +16,23 @@ public class ELineUp extends BattleObj {
 		Limit lim = sb.est.lim;
 		for (int i = 0; i < 2; i++)
 			for (int j = 0; j < 5; j++) {
-				if (lu.fs[i][j] == null) {
+				Form form = lu.fs[i][j];
+				if (form == null) {
 					price[i][j] = -1;
 					continue;
 				}
-				price[i][j] = (int) (lu.efs[i][j].getPrice(sb.st.getCont().price) * 100);
+				if (lim != null && ((lim.line == 1 && i == 1) || lim.unusable(lu.efs[i][j].du, sb.st.getCont().price)))
+					price[i][j] = -1;
+				else
+					price[i][j] = (int) (lu.efs[i][j].getPrice(sb.st.getCont().price) * 100);
 				maxC[i][j] = sb.globalCdLimit() > 0
 						? sb.b.t().getFinResGlobal(sb.globalCdLimit(), sb.isBanned(C_RESP))
 						: sb.b.t().getFinRes(lu.efs[i][j].du.getRespawn(), sb.isBanned(C_RESP));
-				if (lim != null && ((lim.line == 1 && i == 1) || lim.unusable(lu.efs[i][j].du, sb.st.getCont().price)))
-					price[i][j] = -1;
+				if (lim != null && lim.stageLimit != null) {
+					if (price[i][j] != -1)
+						price[i][j] = price[i][j] * lim.stageLimit.costMultiplier[form.unit.rarity] / 100;
+					maxC[i][j] = maxC[i][j] * lim.stageLimit.cooldownMultiplier[form.unit.rarity] / 100;
+				}
 			}
 	}
 
